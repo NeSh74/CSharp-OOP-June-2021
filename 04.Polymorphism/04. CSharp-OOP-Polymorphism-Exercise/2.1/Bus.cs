973da524c3b69b0a288773cd928fc39387a579fe
NeSh74 <nejdie@yahoo.com>
2021-07-08 21:56:42 +0300

    add
    new

---
+++ 04.Polymorphism/04. CSharp-OOP-Polymorphism-Exercise/2.1/Bus.cs
@@ -0,0 +1,22 @@
+﻿namespace VehiclesExtension
+{
+    public class Bus : Vehicle
+    {
+        private const double BusAirConditionModifier = 1.4;
+
+        public Bus(double fuel, double fuelConsumption, double tankCapacity)
+            : base(fuel, fuelConsumption, tankCapacity, BusAirConditionModifier)
+        {
+        }
+
+        public void TurnOnAirConditiner()
+        {
+            this.AirConditionerModifier = BusAirConditionModifier;
+        }
+
+        public void TurnOffAirConditiner()
+        {
+            this.AirConditionerModifier = 0;
+        }
+    }
+}
