fd365d6389cb8c28cb716e149c1873ca04016b6f
NeSh74 <nejdie@yahoo.com>
2021-07-25 16:31:29 +0300

    add
    new

---
+++ 09.Unit Testing/09. CSharp-OOP-Unit-Testing-Lab/UnitTests_Stamo/BankAccount.Test/BankAccountTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace BankAccount.Test
+{
+    [TestFixture]
+
+    public class BankAccountTest
+    {
+        private UnitTests.BankAccount account;
+
+        [SetUp]
+
+        public void InitializeBankAccount()
+        {
+            account = new UnitTests.BankAccount(1000);
+        }
+
+        [Test]
+
+        public void InitializeBankAccountWithAmount()
+        {
+            Assert.That(account.Amount, Is.EqualTo(1000));
+        }
+
+        [Test]
+
+        public void DepositPositiveAmount()
+        {
+            account.Deposit(200);
+            Assert.That(account.Amount, Is.EqualTo(1200));
+        }
+
+        [Test]
+        public void DepositNegativeAmount()
+        {
+            Assert.That(() => account.Deposit(-65), Throws.InvalidOperationException.With.Message
+                .EqualTo("Deposit amount must be more than 0"));
+        }
+    }
+}
---
+++ 09.Unit Testing/09. CSharp-OOP-Unit-Testing-Lab/UnitTests_Stamo/UnitTests/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace UnitTests
+{
+    public class BankAccount
+    {
+        private decimal amount;
+
+        public decimal Amount
+        {
+            get
+            {
+                return amount;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Amount can't be negative");
+                }
+
+                amount = value;
+            }
+        }
+
+        public BankAccount(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public void Deposit(decimal depositAmount)
+        {
+            if (depositAmount <= 0)
+            {
+                throw new InvalidOperationException("Deposit amount must be more than 0");
+            }
+
+            Amount += depositAmount;
+        }
+
+        public void Withdraw(decimal withdrawAmount)
+        {
+            if (withdrawAmount <= 0)
+            {
+                throw new InvalidOperationException("Withdraw amount must be more than 0");
+            }
+
+            Amount -= withdrawAmount;
+        }
+    }
+}
